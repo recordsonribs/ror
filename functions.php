0f453e81f43bd7321e3c67a3cdf0f94c1fdd39ef
Alex <alex@recordsonribs.com>
2014-03-09 16:17:21 +0000

    Remove function causing real problems.

--- functions.php
+++ functions.php
@@ -352,50 +352,6 @@ add_filter('archive_meta', 'wpautop');
 
 // Remember: the Sandbox is for play.
 ?>
-
-
-
-<?php
-    if(!function_exists('how_long_ago')){
-        function how_long_ago($timestamp){
-            $difference = time() - $timestamp;
-
-            if($difference >= 60*60*24*365){        // if more than a year ago
-                $int = intval($difference / (60*60*24*365));
-                $s = ($int > 1) ? 's' : '';
-                $r = $int . ' year' . $s . ' ago';
-            } elseif($difference >= 60*60*24*7*5){  // if more than five weeks ago
-                $int = intval($difference / (60*60*24*30));
-                $s = ($int > 1) ? 's' : '';
-                $r = $int . ' month' . $s . ' ago';
-            } elseif($difference >= 60*60*24*7){        // if more than a week ago
-                $int = intval($difference / (60*60*24*7));
-                $s = ($int > 1) ? 's' : '';
-                $r = $int . ' week' . $s . ' ago';
-            } elseif($difference >= 60*60*24){      // if more than a day ago
-                $int = intval($difference / (60*60*24));
-                $s = ($int > 1) ? 's' : '';
-                $r = $int . ' day' . $s . ' ago';
-            } elseif($difference >= 60*60){         // if more than an hour ago
-                $int = intval($difference / (60*60));
-                $s = ($int > 1) ? 's' : '';
-                $r = $int . ' hour' . $s . ' ago';
-            } elseif($difference >= 60){            // if more than a minute ago
-                $int = intval($difference / (60));
-                $s = ($int > 1) ? 's' : '';
-                $r = $int . ' minute' . $s . ' ago';
-            } else {                                // if less than a minute ago
-                $r = 'moments ago';
-            }
-
-            return $r;
-        }
-    }
-?>
-
-
-
-
 <?php function comments($comment, $args, $depth) {
 	$GLOBALS['comment'] = $comment;
 ?>
